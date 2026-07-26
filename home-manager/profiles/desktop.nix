4f01f35db2cf002fab1893016008a1b3ce7c0f03
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2026-07-26 11:59:24 +0200

    disable broken cobang

--- home-manager/profiles/desktop.nix
+++ home-manager/profiles/desktop.nix
@@ -209,7 +209,7 @@ with lib;
         pavucontrol
         ## Multimedia
         vlc
-        cobang # qr codes
+        # cobang # qr codes
         ## Encryption
         deno # for yt-dlp
         #veracrypt
