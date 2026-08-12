244079722982e7d05f19387dcdf96d3b7699d71b
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2026-08-12 17:45:59 +0200

    bj subdomain

--- machines/nc/configuration.nix
+++ machines/nc/configuration.nix
@@ -429,7 +429,18 @@ in
     # extraConfig = commonExtraConfig;
     #   root = "/var/www/bettuna";
     # };
-    "timertest.${config.link.domain}" = {
+    "bjoern-carlberg.${config.link.domain}" = {
+      quic = true;
+      http3_hq = true;
+      http3 = true;
+      # enableACME = true;
+      serverAliases = [
+      ];
+      useACMEHost = config.link.domain;
+      forceSSL = true;
+      extraConfig = commonExtraConfig;
+      root = "/var/www/aboutme/";
+    };"timertest.${config.link.domain}" = {
       quic = true;
       http3_hq = true;
       http3 = true;
