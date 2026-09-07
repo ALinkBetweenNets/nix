c8b03a81d1d08a44b589c2195df0b2748cabe1a6
ALinkbetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2026-09-07 16:45:19 +0200

    fix i2p syntax

--- modules/services/i2p.nix
+++ modules/services/i2p.nix
@@ -41,14 +41,14 @@ in {
         ];
         services.i2pd = {
           enable = true;
-          address =
-            "127.0.0.1"; # you may want to set this to 0.0.0.0 if you are planning to use an ssh tunnel
-          proto = {
-            http.enable = true;
-            socksProxy.enable = true;
-            httpProxy.enable = true;
-            sam.enable = true;
-          };
+          # address =
+            # "127.0.0.1"; # you may want to set this to 0.0.0.0 if you are planning to use an ssh tunnel
+          # proto = {
+          #   http.enable = true;
+          #   socksProxy.enable = true;
+          #   httpProxy.enable = true;
+          #   sam.enable = true;
+          # };
         };
         services.kubo = { enable = true; };
       };
